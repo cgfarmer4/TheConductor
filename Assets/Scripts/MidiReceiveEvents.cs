8f9700e945ae88618a18468d8aef4aff6512defb
Bob Dole <cgfarmer4@gmail.com>
2017-10-04 16:05:43 -0700

    Inktober Day4

---
+++ Assets/Scripts/MidiReceiveEvents.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityOSC;
+using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class MidiReceiveEvents : MonoBehaviour
+{
+    // Class declaration
+    [System.Serializable]
+    public class NoteEvent : UnityEvent<float> { }
+
+    public String OSCReceiveName;
+    public NoteEvent NoteOn;
+    public NoteEvent NoteOff;
+
+   public void Notes(List<object> data)
+    {
+        if (data[2].ToString() == OSCReceiveName)
+        {
+            Debug.Log("I'm an event " + data[2]);
+
+            float pitch = float.Parse(data[0].ToString());
+            float velocity = float.Parse(data[1].ToString());
+
+            if (velocity > 0)
+            {
+                NoteOn.Invoke(pitch);
+            }
+            else
+            {
+                NoteOff.Invoke(pitch);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MidiSendEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityOSC;
+using System;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class MidiSendEvents : MonoBehaviour
+{
+    public String OSCSendName;
+    public float Pitch = 60f;
+    public float Velocity = 100f;
+    public float Duration = 100f;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit me!");
+
+        List<object> midiData = new List<object>();
+        midiData.Add(Pitch); // Pitch
+        midiData.Add(Velocity); // Velocity
+        midiData.Add(Duration); // Duration
+        midiData.Add(OSCSendName); //Object name
+
+        OSCHandler.Instance.SendMessageToClient("myClient", "/midiNote", midiData);
+    }
+}
